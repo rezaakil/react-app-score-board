8458b4e1748b9f38360c69e2f875da1a8caebcb8
REZA AKIL <akil.reza@gmail.com>
2019-05-06 12:26:18 +0200

    1st change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World !
+        Welcome to Codaisseur
         </p>
         <a
           className="App-link"
