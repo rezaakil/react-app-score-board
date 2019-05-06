7504b91fbbdffec8b5cf0b70ae5f5a396e6b4e91
REZA AKIL <akil.reza@gmail.com>
2019-05-06 14:11:49 +0200

    update title and lightbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
+import LightSwitch from './components/LightSwitch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Welcome to Codaisseur
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React at Codaisseur
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <main>
+          <Title content="Some Simple Title" />
+          <LightSwitch />
+        </main>
+      </div>
+    );
+  }
 }
-
 export default App;
+
